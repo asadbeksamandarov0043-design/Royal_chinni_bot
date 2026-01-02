05ba3b5e415bb87c192865c915a19ed39544def3
asadbeksamandarov0043-design <asadbeksamandarov0043@gmail.com>
2026-01-02 13:26:33 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,22 +1,36 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    MessageHandler,
+    ContextTypes,
+    filters
+)
 
-TOKEN="8485717621:AAFG-uTaq3OBbMis0tBVNxRZVDbKOZos4hA"
+TOKEN = "8485717621:AAFG-uTaq3OBbMis0tBVNxRZVDbKOZos4hA"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print("START keldi")
-    await update.message.reply_text("Royal Chinni bot ishlayapti ✅")
+    await update.message.reply_text(
+        "Salom 👋\n"
+        "Men Royal Chinni botman.\n"
+        "Menga xabar yozing, men javob beraman 🙂"
+    )
+
+async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_text = update.message.text
+    await update.message.reply_text(
+        f"Siz yozdingiz: {user_text}"
+    )
 
 def main():
     print("BOT ISHGA TUSHDI")
+
     app = ApplicationBuilder().token(TOKEN).build()
+
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat))
+
     app.run_polling()
 
-if__name__=="__main__": 
-
-
-
-
-
+if __name__ == "__main__":
     main()
